f4b26e3cdef64f430c6c2274c30ff7b86be5d8b7
MonkAshish <110713754+MonkAshish@users.noreply.github.com>
2022-08-06 16:08:43 +0530

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	cout << "PLEASE INSERT TWO NUMBER FOR THEIR ADDITION \n";
+	int x,y;
+	cout << "ENTER YOUT FIRST NUMBER \n";
+	cin >> x;
+	cout << "ENTER YOUR SECOND NUMBER \n\n";
+	cin >> y;
+	cout << "YOUR NUMBER AFTER ADDITION IS " << x + y << endl;
+
+	string ashish = "ashisha";
+	cout << ashish;
+
+	return 0;
+
+
+}
